7a822874c9bcc76c046061502e06d5efd02c0708
Diego Dias <diegodias@Diegos-MacBook-Pro.local>
2023-02-08 12:42:47 -0300

    Adding tests successfuly

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,23 +4,36 @@ import {
   NotAcceptableException,
 } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { BcryptService, IBcryptService } from '../bcrypt/bcrypt.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { Role } from './entities/user.entity';
+import { Role, UserEntity } from './entities/user.entity';
 import { UserService } from './user.service';
-
-jest.mock('bcrypt');
+import { faker } from '@faker-js/faker';
 
 describe('UserService', () => {
   let service: UserService;
   let prismaService: PrismaService;
+  let bcryptService: IBcryptService;
+
+  const user = new UserEntity({
+    name: faker.name.firstName(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    role: Role.user,
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, PrismaService],
+      providers: [UserService, PrismaService, BcryptService],
     }).compile();
 
     service = module.get<UserService>(UserService);
     prismaService = module.get<PrismaService>(PrismaService);
+    prismaService.user.findMany = jest.fn().mockReturnValue([]);
+    prismaService.user.create = jest.fn().mockReturnValue(user);
+    bcryptService = module.get<IBcryptService>(BcryptService);
+    bcryptService.compare = jest.fn();
+    bcryptService.hash = jest.fn().mockReturnValue('stringsecret');
   });
 
   it('should be defined', () => {
@@ -38,13 +51,13 @@ describe('UserService', () => {
     ).rejects.toBeInstanceOf(HttpException);
   });
   it('should create successfully', async () => {
-    await expect(
+    expect(
       service.create({
-        email: 'diegodias@testshouldpass.com',
-        name: 'Diego Dias Pass test',
+        email: 'diegodias@testshouldpass.commm',
+        name: 'Diego Dias Pass tesssst',
         password: 'passwordrandmonpasstest',
         role: Role.admin,
       }),
-    ).resolves.toEqual(true);
+    ).resolves.toEqual(user);
   });
 });
