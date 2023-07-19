20a6b90d207fc50e4487fd15c8e7e80a3606639d
Diego Dias <ddiasfront@gmail.com>
2023-07-19 07:37:38 -0300

    Added controller providers and in testing file and future streaming endpoint

--- src/user/users.controller.spec.ts
+++ src/user/users.controller.spec.ts
@@ -1,11 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { BcryptService } from '../bcrypt/bcrypt.service';
+import { EmailValidationService } from '../email/emailValidation.service';
+import { JWTService } from '../jwt/jwt.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        PrismaService,
+        BcryptService,
+        EmailValidationService,
+        JWTService,
+        ConfigService,
+      ],
       controllers: [UsersController],
     }).compile();
 
